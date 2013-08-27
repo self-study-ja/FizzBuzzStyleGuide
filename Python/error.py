f1b58d2a855c1be846b2e203da813e79600d28f4
esehara <esehara@gmail.com>
2013-08-28 08:18:50 +0900

    ADD: Python: Error FizzBuzz

---
+++ Python/error.py
@@ -0,0 +1,23 @@
+def error_fizzbuzz(x):
+    fizz, buzz = "", ""
+
+    try:
+        fizz = ["Fizz"][x % 3]
+    except:
+        pass
+
+    try:
+        buzz = ["Buzz"][x % 5]
+    except:
+        pass
+    
+    try:
+        (fizz + buzz)[1]
+        print fizz + buzz
+    except:
+        print x
+
+
+if __name__ == '__main__':
+    for x in range(1, 100):
+        error_fizzbuzz(x)
